55b467136a3f550890fa5149498280a71dac6fe3
babuamrit <92739498+babuamrit@users.noreply.github.com>
2022-06-09 07:44:58 +0545

    Revert "f"
    This reverts commit 0029b2647dcc9a02b295922601c800de0c295f0e.

--- src/Pages/Editor.js
+++ src/Pages/Editor.js
@@ -17,7 +17,6 @@ export const Editor = () => {
    <Sidebar> 
        <div>Enter the following information</div>
        <div className='editor_gridcontainer'>
-         <div className="editor_flex">
            {items[params.name].map((value,index)=>(
                <div class="mb-4 editor_input_grid">
                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
@@ -32,11 +31,6 @@ export const Editor = () => {
              
            ))}
        </div>
-       <div>
-         <input type="file" accept="image/file" />
-       </div>
-       </div>
-       
    </Sidebar>
   )
 }
